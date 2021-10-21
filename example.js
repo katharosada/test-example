298336b5217c468f79b457c132ca6d56fd425da9
Katie Bell <katie@katharos.id.au>
2021-10-21 17:03:05 +1100

    Use better example function

--- example.js
+++ example.js
@@ -1,7 +1,22 @@
 
-
-function myFunc() {
-    return "hello there"
+function mostCommonWord(words) {
+    const counts = {}
+    words.forEach(word => {
+        if (counts[word]) {
+            counts[word] += 1
+        } else {
+            counts[word] = 1
+        }
+    })
+    let maxWord = '';
+    let maxCount = 0
+    for (let word in counts) {
+        if (counts[word] > maxCount) {
+            maxCount = counts[word]
+            maxWord = word
+        }
+    }
+    return maxWord
 }
 
-module.exports = myFunc
+module.exports = mostCommonWord
--- example.test.js
+++ example.test.js
@@ -1,7 +1,32 @@
 
-const myFunc = require('./example')
+const mostCommonWord = require('./example')
 
-test('myFunc returns hello', () => {
-    const result = myFunc()
-    expect(result).toBe("hello there");
+test('mostCommonWord returns only word', () => {
+    const result = mostCommonWord(["hello"])
+    expect(result).toBe("hello");
 });
+
+test('mostCommonWord returns only word when there are two', () => {
+    const result = mostCommonWord(["hello", "hello"])
+    expect(result).toBe("hello");
+});
+
+test('mostCommonWord is hello when there are two hellos and one other word', () => {
+    const result = mostCommonWord(["hello", "hello", "other"])
+    expect(result).toBe("hello");
+});
+
+test('mostCommonWord is hello when there are two hellos and one other word', () => {
+    const result = mostCommonWord(["other", "hello", "hello"])
+    expect(result).toBe("hello");
+});
+
+test('mostCommonWord is something when there are lots of duplicates', () => {
+    const result = mostCommonWord(["other", "something", "hello", "hello", "other", "something", "something"])
+    expect(result).toBe("something");
+});
+
+test('mostCommonWord when there is a tie, should return the first one from the list', () => {
+    const result = mostCommonWord(["other", "hello", "hello", "other"])
+    expect(result).toBe("other")
+})
